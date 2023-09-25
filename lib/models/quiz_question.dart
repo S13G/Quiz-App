637d57f6f954660ea4f26ecfedb52356958c8551
s13g <ayflix0@gmail.com>
2023-09-25 17:01:37 +0100

    rendered questions and shuffled answers

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -3,4 +3,12 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  // Creates a copy of the answers list and shuffles it
+  // final only makes sure you can't reassign the variable
+  List<String> getShuffledAnswers() {
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
