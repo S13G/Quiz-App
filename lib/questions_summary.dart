0fc90270d3e04af65cfce357f2569897e2decaf0
s13g <ayflix0@gmail.com>
2023-10-01 20:51:51 +0100

    results screen is functional

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsSummary extends StatelessWidget {
   const QuestionsSummary({super.key, required this.summaryData});
@@ -13,21 +14,68 @@ class QuestionsSummary extends StatelessWidget {
         child: Column(
           children: summaryData.map(
                 (data) {
-              return Row(
+              bool isCorrect = data['user_answer'] == data['correct_answer'];
+              return Column(
                 children: [
-                  Text(((data['question_index'] as int) + 1).toString()),
-                  Expanded(
-                    child: Column(
-                      children: [
-                        Text(data['question'] as String),
-                        const SizedBox(
-                          height: 5,
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor:
+                        isCorrect ? Colors.greenAccent : Colors.red,
+                        radius: 16.0,
+                        child: Text(
+                          ((data['question_index'] as int) + 1).toString(),
+                          style: GoogleFonts.inter(
+                            color: Colors.black45,
+                            fontWeight: FontWeight.w900,
+                          ),
                         ),
-                        Text(data['user_answer'] as String),
-                        Text(data['correct_answer'] as String),
-                      ],
-                    ),
-                  )
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              data['question'] as String,
+                              style: GoogleFonts.inter(
+                                color: Colors.white54,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              data['user_answer'] as String,
+                              textAlign: TextAlign.left,
+                              style: GoogleFonts.inter(
+                                color: Colors.white30,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 17,
+                              ),
+                            ),
+                            Text(
+                              data['correct_answer'] as String,
+                              textAlign: TextAlign.left,
+                              style: GoogleFonts.inter(
+                                color: Colors.blueGrey,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 17,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10, // Adjust the height for spacing between questions
+                  ),
                 ],
               );
             },
